f02087e4faca30ddf6a96728ccb3cd6264f08711
vinay duvvada <vinay.chowdary518@gmail.com>
2017-12-11 22:31:25 -0800

    added servlet for product creation and display

---
+++ productweb/src/main/java/com/fuppino/product/servlet/CreateProductServlet.java
@@ -0,0 +1,16 @@
+package com.fuppino.product.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ productweb/src/main/java/com/fuppino/product/servlet/DisplayProductServlet.java
@@ -0,0 +1,16 @@
+package com.fuppino.product.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DisplayProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
