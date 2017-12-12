a70b5cb79b752843df7d6f20415d9031cbc306c6
vinay duvvada <vinay.chowdary518@gmail.com>
2017-12-12 00:55:36 -0800

    loading service components in web application using spring web application context

--- productweb/src/main/java/com/fuppino/product/servlet/CreateProductServlet.java
+++ productweb/src/main/java/com/fuppino/product/servlet/CreateProductServlet.java
@@ -1,16 +1,36 @@
 package com.fuppino.product.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.fuppino.product.bo.ProductBO;
+import com.fuppino.product.dto.Product;
+
 public class CreateProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer productId = Integer.parseInt(request.getParameter("id"));
+		String productName = request.getParameter("name");
+		String productDescription =request.getParameter("description");
+		Integer productPrice = Integer.parseInt(request.getParameter("price"));
 		
+		Product product = new Product(productId, productName, productDescription, productPrice);
+		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO productBO = (ProductBO) context.getBean("productbo");
+		productBO.create(product);
+		
+		PrintWriter out = response.getWriter();
+		out.print("product created!!");
 	}
 
 }
--- productweb/src/main/java/com/fuppino/product/servlet/DisplayProductServlet.java
+++ productweb/src/main/java/com/fuppino/product/servlet/DisplayProductServlet.java
@@ -1,16 +1,33 @@
 package com.fuppino.product.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.fuppino.product.bo.ProductBO;
+import com.fuppino.product.dto.Product;
+
 public class DisplayProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO productBO = (ProductBO) context.getBean("productbo");
+		Product product = productBO.findProduct(Integer.parseInt(request.getParameter("id")));
+		
+		PrintWriter out = response.getWriter();
+		out.print("product details");
+		out.print("product ID: "+product.getId());
+		out.print("product Name: "+product.getName());
+		out.print("product Description: "+product.getDescription());
+		out.print("product price: "+product.getPrice());
 	}
 
 }
